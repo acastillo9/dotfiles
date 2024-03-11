6b62dfc6ef1995a6eb5db2bd37bbf00078b00a7d
Andrés Castillo <info@acastillo.dev>
2024-03-10 21:03:43 -0500

    some improvements on fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,21 @@
 ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
 set --export --prepend PATH "/Users/andrescastillo/.rd/bin"
 ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
-set -gx EDITOR nvim
 
 starship init fish | source
+
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
 
 ### NVM
 set -gx NVM_DIR (brew --prefix nvm)
