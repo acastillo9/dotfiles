7ec38b9e2a814c6e566564a6393c630a1bc0ad0c
Andrés Castillo <info@acastillo.dev>
2024-03-20 14:11:51 -0500

    tmux, wezterm configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,5 +5,11 @@ config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
 
 return config
